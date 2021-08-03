876628081a396e330a751bd9cc3746c2ee53466e
Luozhihao0821 <71737492+Luozhihao0821@users.noreply.github.com>
2021-08-03 18:02:36 +0800

    变量

---
+++ Java/20210802/src/hello.java
@@ -0,0 +1,9 @@
+/*Javaª∑æ≥≤‚ ‘*/
+public class hello {
+   public static void main(String[] args) {
+      for (int i = 0; i<10; i++) {
+    	  System.out.println(i);
+      }
+      System.out.println("Hello World!");
+   }
+}
---
+++ Java/20210803/src/pro02/pro02.java
@@ -0,0 +1,20 @@
+package pro02;
+
+public class pro02 {
+	int b;//实例成员变量
+	static int c;//静态成员变量
+
+	public static void main(String[] args) {
+//		1.变量的定义
+		int a;//局部变量
+		boolean flag;
+		
+//		2.变量赋值
+		a=10;
+		System.out.println(a);
+		flag=true;
+		System.out.println(flag);
+
+	}
+
+}
